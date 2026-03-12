c25339bdd895f0e4e11c456a62e7fa8a75bb2ab3
Kavyan Nayak <kavyannayak104@gmail.com>
2026-03-12 19:21:13 +0530

    if-esle

---
+++ If-else/if-else.py
@@ -0,0 +1,6 @@
+age = 16
+
+if age >= 18:
+    print("You can Drive a car.")
+else:
+    print("You cannot Drive a car.")
---
+++ If-else/user.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number to check the number is odd or even : "))
+
+
+if num % 2 == 0:
+        print("The number is even.")
+        
+else :
+        print("The number is odd.")
