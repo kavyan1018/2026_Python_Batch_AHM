107b6b71b77b40964e1f1ab1ffcd2466d0d06a93
Kavyan Nayak <kavyannayak104@gmail.com>
2026-03-26 19:41:34 +0530

    match

---
+++ Switch/match.py
@@ -0,0 +1,12 @@
+a = int(input("1. Addition 2. subtraction 3.Multiplication 4. Division 5.Exit.."))
+match a:
+    case 1:
+        print("1.. Addition")    
+    case 2:
+        print("2.. Subtraction")    
+    case 3:
+        print("3.. Multiplication")    
+    case 4:
+        print("4.. Division")
+    case _:
+        print("Exit ")
