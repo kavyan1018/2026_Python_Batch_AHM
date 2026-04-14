bd13825617c138b4af501871756d8c0456650b98
Kavyan Nayak <kavyannayak104@gmail.com>
2026-04-14 19:28:07 +0530

    while loop

---
+++ Loops/while.py
@@ -0,0 +1,20 @@
+# while condition:
+#     # code block
+
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+    
+    
+    """
+    password = ""
+
+while password != "admin":
+    password = input("Enter password: ")
+
+print("Access Granted")
+    """
+    
