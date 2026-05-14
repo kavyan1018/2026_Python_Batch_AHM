93b270ac1f790bf5a4428798562c2ea5db407a2a
Kavyan Nayak <kavyannayak104@gmail.com>
2026-05-14 19:38:48 +0530

    is methods

---
+++ String/ismethod.py
@@ -0,0 +1,48 @@
+
+# -> bolean
+# true
+# false
+
+# cheking Lower case
+# text = "python"
+# print(text.islower())
+
+
+# cheking upper case
+# text = "PYTHOn"
+# print(text.isupper())
+
+
+# cheking space
+# text = "Python"
+# print(text.isspace())
+
+
+# text + num 
+# text = "Python"
+# text = "123"
+# text = "python14"
+# print(text.isalnum())
+
+# it returns only alphabates
+# text = "Python"
+# print(text.isalpha())
+
+# number check 
+# text = "1234"
+# print(text.isdigit())
+# text = "1234"
+# print(text.isnumeric())
+# text = "56"
+# print(text.isdecimal()) # 0 - 9
+
+
+# text = "Student123"
+# print(text.isidentifier())
+
+
+# text = "1"
+# print(text.isprintable())
+
+text = ""
+print(text.isascii())
