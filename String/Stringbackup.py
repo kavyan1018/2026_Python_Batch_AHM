98eac2140c4f72530c707d01bac4e5d2bef41d62
Kavyan Nayak <kavyannayak104@gmail.com>
2026-05-19 18:39:15 +0530

    string backup

---
+++ String/Stringbackup.py
@@ -0,0 +1,20 @@
+# string is a immutable sequence of characters.
+
+# immutable -> cannot be changed
+    #   012345
+name = "Kavyan"
+print(name)
+
+
+# 1 char - acc to index  -> index -> array  0, 1, 2, 3, 4, 5
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3]) 
+
+for i in name:
+    print(i)
+    
+
+# length of the string
