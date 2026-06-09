81db60b933e8deea15fbff7ef58f40ff3a8b3ce9
Kavyan Nayak <kavyannayak104@gmail.com>
2026-06-09 19:00:33 +0530

    type casting

---
+++ ds/tuple/Typecast.py
@@ -0,0 +1,20 @@
+# type casting -> means convert one data type to another data type
+
+data = ("ram", "shyam", "hari", "gopal")
+print(data)
+print(type(data))
+
+
+
+# tuple -> list 
+lst = list(data)
+print(type(lst))
+
+lst[3] = "sita"
+print(lst)
+
+
+# List -> tuple
+tup = tuple(lst)
+print(tup)
+print(type(tup))
