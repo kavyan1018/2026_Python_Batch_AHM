6dca1e11d76f734074c262c79b5f0c4597957d4d
Kavyan Nayak <kavyannayak104@gmail.com>
2026-06-09 19:00:28 +0530

    tuple demo 2

---
+++ ds/tuple/tupleDemo2.py
@@ -0,0 +1,23 @@
+# tup1 = (1, 2, 3)
+# tup2 = (4, 5, 6)
+
+# tup3 = tup1 + tup2
+
+# print(tup3)
+
+# data = ()
+# print(type(data))
+# print(data)
+
+# a = (1, 2, 3, 4, 5)
+# a[0] = 10
+# print(a)
+
+
+sales = ([100, 200, 300], 
+         [400, 500, 600], 
+         [700, 800, 900])
+
+# print(sales)
+sales[1][1] = 550
+print(sales)
