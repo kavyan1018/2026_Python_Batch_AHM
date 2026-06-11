2bd2f52bf1869432c1df8ae96787bacf7ea481de
Kavyan Nayak <kavyannayak104@gmail.com>
2026-06-11 18:48:18 +0530

    set oprators

---
+++ ds/set/setDemo2.py
@@ -0,0 +1,26 @@
+    
+"""
+
+    | -> Union  ==> take all unique elements from both sets
+    & -> Intersection ==> take only common elements from both sets
+    - -> Difference ==> take only unique elements from first set
+    ^ -> Symmetric Difference ==> take only unique elements in either set but not in both sets
+    x in a -> membership operator ==> check if x is present in set a or not
+    x not in a -> membership operator ==> check if x is not present in set a or not
+"""
+
+
+a = {10, 20, 30, 40}
+b = {30, 40, 50, 60}
+
+# print("Union:", a | b)
+
+# print("Intersection:", a & b)
+
+# print("Difference:", a - b)
+
+# print("Symmetric Difference:", a ^ b)
+
+# print("Membership check:", 70 in a)
+
+# print("Membership check:", 20 not in a)
