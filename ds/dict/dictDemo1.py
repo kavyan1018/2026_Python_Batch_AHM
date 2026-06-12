4d8a8d23f77b84a8d79871e75c7cc7d27613a24e
Kavyan Nayak <kavyannayak104@gmail.com>
2026-06-12 13:32:46 +0530

    basics of Dictionary

---
+++ ds/dict/dictDemo1.py
@@ -0,0 +1,23 @@
+student = {
+    "name" : "Kavyan",
+    "age" : 22,
+    "course" : "Python",
+    "Fees" : 10000
+}
+
+print(student)
+# access 
+# print(student.get("name"))
+
+
+# keys access
+# k = student.keys()
+# print(k)
+
+# values access
+# v = student.values()
+# print(v)
+
+# kv = student.items()
+# print(kv)
+
