0bdc89912f3c219f623f4b767068495edadaa7ce
Kavyan Nayak <kavyannayak104@gmail.com>
2026-06-22 18:20:31 +0530

    Function Demo 1

---
+++ Functions/FunctionDemo1.py
@@ -0,0 +1,25 @@
+# no1 = 100
+# no2 = 200
+
+# print("Before Swapping: no1 =", no1, "no2 =", no2)
+
+"""
+syntax of function definition
+
+def -> keyword to define a function
+function_name -> name of the function
+"""
+
+# def greet():
+#     print("Hello, welcome to Python programming!")
+    
+# greet()  # calling the function
+
+
+
+# function with parameters
+
+def add(a, b):
+    print("Sum =", a + b)
+    
+add(10, 20)
